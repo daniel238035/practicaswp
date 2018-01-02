2cc6ed438736601752067c0f007bce0118a99ed4
DANIELCOREI7\Daniel <daniel238035@hotmail.com>
2018-01-02 18:35:31 -0500

    otras convocatorias page y single

--- wp-content/themes/ogsiatema/listaotrasconv-page.php
+++ wp-content/themes/ogsiatema/listaotrasconv-page.php
@@ -17,7 +17,7 @@ Template Name: Página Otras Convocatorias
           <div class="row">
             <div class="col s12 l5 offset-l1">
               <h2 class="white-text"><?php the_title() ?></h2>
-              <p class="white-text">Aquí podrás encontrar información sobre los programas nacionales e internacionales de formación e intercambios de carácter universitario. </p>
+              <p class="white-text"><?php the_field( 'descripcion_movilidad_estudiantil' ); ?></p>
             </div>
           </div>
       </div>
@@ -27,15 +27,27 @@ Template Name: Página Otras Convocatorias
   <div class="row  white seccion">
             <div class="container">
               <div class="row">
+
+                <!--LOOP Wordpress Inicio-->
+                <?php
+                      query_posts(array(
+                        "showposts" => 18,
+                        "cat" => 6,
+                        //'post_type'=>'',
+                        'order' => 'DES'
+                        ));
+                ?>
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
                 <div class="col s12 m4">
                   <div class="card" style="padding:0px!important;">
                     <div class="card-image waves-effect waves-block waves-light">
-                      <img class="activator" src="images/portadas/portada3.jpg">
+                      <img class="activator" src="<?php bloginfo('template_url')?>/images/portadas/portada3.jpg">
                     </div>
                     <div class="card-content" style="padding:15px!important;">
                       <div class="row flex-row">
                         <div class="col s10">
-                          <h5 class="titulo-borde-izquierdo-2 activator">Nombre de la Convocatoria</h5>
+                          <h5 class="titulo-borde-izquierdo-2 activator"><?php the_title() ?></h5>
                         </div>
                         <div class="col s2">
                           <span class="card-title activator grey-text text-darken-4"><i class="material-icons right">more_vert</i></span>
@@ -48,7 +60,7 @@ Template Name: Página Otras Convocatorias
                                 <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Institución">place</i>
                               </div>
                               <div class="col s10">
-                                Universidad Nacional Pedro Ruiz Gallo
+                                <?php the_field( 'institucion' ); ?>
                               </div>
                           </div>
                         </div>
@@ -58,7 +70,7 @@ Template Name: Página Otras Convocatorias
                                 <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Inicio">event</i>
                               </div>
                               <div class="col s10">
-                                18 de agosto del 2017
+                                <?php the_field( 'inicio_de_convocatoria' ); ?>
                               </div>
                           </div>
                         </div>
@@ -68,7 +80,7 @@ Template Name: Página Otras Convocatorias
                                 <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Duración">date_range</i>
                               </div>
                               <div class="col s10">
-                                3 años
+                                <?php the_field( 'duracion_de_convocatoria' ); ?>
                               </div>
                           </div>
                         </div>
@@ -76,130 +88,22 @@ Template Name: Página Otras Convocatorias
 
                     </div>
                     <div class="card-action" style="padding: 5px 5px!important">
-                      <a class="btn btn-flat green-text text-negrita" href="http://localhost/practicas/movilidad.php" >Inscríbete</a>
+                      <a class="btn btn-flat green-text text-negrita" href="<?php the_permalink() ?>" >Inscríbete</a>
                     </div>
                     <div class="card-reveal">
-                      <span class="card-title grey-text text-darken-4">Nombre de la Convocatoria<i class="material-icons right">close</i></span>
-                      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                      <span class="card-title grey-text text-darken-4"><?php the_title() ?><i class="material-icons right">close</i></span>
+                      <div><?php the_field( 'presentacion' ); ?></div>
                     </div>
                   </div>
                 </div>
 
-                <div class="col s12 m4">
-                  <div class="card" style="padding:0px!important;">
-                    <div class="card-image waves-effect waves-block waves-light">
-                      <img class="activator" src="images/portadas/portada3.jpg">
-                    </div>
-                    <div class="card-content" style="padding:15px!important;">
-                      <div class="row flex-row">
-                        <div class="col s10">
-                          <h5 class="titulo-borde-izquierdo-2 activator">Nombre de la Convocatoria</h5>
-                        </div>
-                        <div class="col s2">
-                          <span class="card-title activator grey-text text-darken-4"><i class="material-icons right">more_vert</i></span>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col s12" style="margin: 8px 0px!important;">
-                          <div class="row" >
-                              <div class="col s2 flex-row-start">
-                                <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Institución">place</i>
-                              </div>
-                              <div class="col s10">
-                                Universidad Nacional Pedro Ruiz Gallo
-                              </div>
-                          </div>
-                        </div>
-                        <div class="col s12" style="margin: 8px 0px!important;">
-                          <div class="row">
-                              <div class="col s2 flex-row-start">
-                                <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Inicio">event</i>
-                              </div>
-                              <div class="col s10">
-                                18 de agosto del 2017
-                              </div>
-                          </div>
-                        </div>
-                        <div class="col s12" style="margin: 8px 0px!important;">
-                          <div class="row">
-                              <div class="col s2 flex-row-start">
-                                <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Duración">date_range</i>
-                              </div>
-                              <div class="col s10">
-                                3 años
-                              </div>
-                          </div>
-                        </div>
-                      </div>
-
-                    </div>
-                    <div class="card-action" style="padding: 5px 5px!important">
-                      <a class="btn btn-flat green-text text-negrita" href="http://localhost/practicas/movilidad.php" >Inscríbete</a>
-                    </div>
-                    <div class="card-reveal">
-                      <span class="card-title grey-text text-darken-4">Nombre de la Convocatoria<i class="material-icons right">close</i></span>
-                      <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col s12 m4">
-                  <div class="card" style="padding:0px!important;">
-                    <div class="card-image waves-effect waves-block waves-light">
-                      <img class="activator" src="images/portadas/portada3.jpg">
-                    </div>
-                    <div class="card-content" style="padding:15px!important;">
-                      <div class="row flex-row">
-                        <div class="col s10">
-                          <h5 class="titulo-borde-izquierdo-2 activator">Nombre de la Convocatoria</h5>
-                        </div>
-                        <div class="col s2">
-                          <span class="card-title activator grey-text text-darken-4"><i class="material-icons right">more_vert</i></span>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col s12" style="margin: 8px 0px!important;">
-                          <div class="row" >
-                              <div class="col s2 flex-row-start">
-                                <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Institución">place</i>
-                              </div>
-                              <div class="col s10">
-                                Universidad Nacional Pedro Ruiz Gallo
-                              </div>
-                          </div>
-                        </div>
-                        <div class="col s12" style="margin: 8px 0px!important;">
-                          <div class="row">
-                              <div class="col s2 flex-row-start">
-                                <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Inicio">event</i>
-                              </div>
-                              <div class="col s10">
-                                18 de agosto del 2017
-                              </div>
-                          </div>
-                        </div>
-                        <div class="col s12" style="margin: 8px 0px!important;">
-                          <div class="row">
-                              <div class="col s2 flex-row-start">
-                                <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Duración">date_range</i>
-                              </div>
-                              <div class="col s10">
-                                3 años
-                              </div>
-                          </div>
-                        </div>
-                      </div>
-
-                    </div>
-                    <div class="card-action" style="padding: 5px 5px!important">
-                      <a class="btn btn-flat green-text text-negrita" href="http://localhost/practicas/movilidad.php" >Inscríbete</a>
-                    </div>
-                    <div class="card-reveal">
-                      <span class="card-title grey-text text-darken-4">Nombre de la Convocatoria<i class="material-icons right">close</i></span>
-                      <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                    </div>
-                  </div>
-                </div>
+                <?php endwhile; ?>
+                <!-- post navigation -->
+                <?php else: ?>
+                <!-- no posts found -->
+                <?php endif; ?>
+                <?php wp_reset_query(); ?>
+                <!--LOOP Wordpress Fin-->
 
               </div>
             </div>
